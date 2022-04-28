ccfaf52dbd8a94ec5725cec1ef0bf7771675993f
Bipasha Agarwal <agarwalbipasha@gmail.com>
2022-04-28 16:00:11 +0530

    Fixed the checkbox

--- app.js
+++ app.js
@@ -1,48 +1,99 @@
-var form = document.getElementById('input-area');
+var form = document.getElementById("addForm");
 // console.log(form);
-var itemList = document.getElementById('items');
+var itemList = document.getElementById("items");
 
-//Form submit event
-form.addEventListener('keypress', addItem);
+form.addEventListener("keypress", addItem);
+itemList.addEventListener("click", removeItem);
 
-itemList.addEventListener('click', removeItem);
+var arrayOfItems = [];
+console.log(arrayOfItems);
+console.log(arrayOfItems.length);
 
-
-//Add Item
 function addItem(event) {
+  if (event.keyCode === 13) {
     event.preventDefault();
-    
-    if (event.key === 'Enter') {
+    var newItem = document.querySelector(".input-area").value;
+    if (newItem != "" && newItem != " ") {        
+      var li = document.createElement("li");
+      li.classList.add("list-group-item");
+      var div = document.createElement("div");
+      div.appendChild(document.createTextNode(newItem));
+      arrayOfItems.push(newItem);
+      console.log(arrayOfItems);
+      var itemLength = document.querySelector('.item-count');
+      console.log(itemLength);
+      if (arrayOfItems.length < 2) {
+        itemLength.textContent = `${arrayOfItems.length}` + " item left"; 
+      } else {
+        itemLength.textContent = `${arrayOfItems.length}` + " items left";
+      }
+      document.querySelector(".input-area").value = "";
+      li.appendChild(div);
+      var deleteButton = document.createElement("button");
+      deleteButton.classList.add("delete");
+      deleteButton.appendChild(document.createTextNode("X"));
+      li.appendChild(deleteButton);
+      var tickBoxContainer = document.createElement('div');
+      tickBoxContainer.classList.add('container');
+      var roundBox = document.createElement('div');
+      roundBox.classList.add('round');
+      var checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = 'checkbox';
+      checkbox.checked = false;
+      var labelCheckbox = document.createElement('label');
+      labelCheckbox.htmlFor = 'checkbox';
+      labelCheckbox.classList.add('checkbox');
+      roundBox.appendChild(checkbox);
+      roundBox.appendChild(labelCheckbox);
+      tickBoxContainer.appendChild(roundBox);
+      li.insertAdjacentElement('afterbegin', tickBoxContainer);
 
-        var newItem = document.getElementById('input-area').value;
-        console.log(newItem);
-        if (newItem != "" || newItem != " ") {
-        console.log("inside");
-        var li = document.createElement('li');
-        li.classList.add('list-group-item');
-        console.log(li);
-        var div = document.createElement('div');
-        div.appendChild(document.createTextNode(newItem));
-        document.querySelector('.input-area').value = "";
-        li.appendChild(div);
-        // console.log(div);
-        // console.log(li);
-        var deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete');
-        deleteButton.appendChild(document.createTextNode('X'));
-        li.appendChild(deleteButton);
-        itemList.appendChild(li);
-        }
+      tickBoxContainer.addEventListener('click', checkItem);
+      itemList.appendChild(li);
     }
+  }
 }
 
 //remove item
-
 function removeItem(event) {
-    if(event.target.classList.contains('delete')) {
-        if(confirm('Are you sure?')) {
-            var li = event.target.parentElement;
-            itemList.removeChild(li);
-        }
+  if (event.target.classList.contains("delete")) {
+    if (confirm("Are you sure?")) {
+      var li = event.target.parentElement;
+      console.log(li.children[1].textContent);
+      console.log(arrayOfItems.indexOf(li.children[1].textContent));
+      // console.log(arrayOfItems);
+      itemList.removeChild(li);
+      delete arrayOfItems[arrayOfItems.indexOf(li.children[1].textContent)];
+      arrayOfItems.filter(Boolean);
+      console.log(arrayOfItems);
+      console.log(arrayOfItems.length);
+      var itemLength = document.querySelector('.item-count');
+      console.log(itemLength);
+      if (arrayOfItems.length < 2) {
+        itemLength.textContent = `${arrayOfItems.length}` + " item left"; 
+      } else {
+        itemLength.textContent = `${arrayOfItems.length}` + " items left";
+      }
+      console.log(itemLength);
     }
+  }
 }
+
+//line through item
+function checkItem(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("checkbox")) {
+    let targetItem = event.target.parentElement.parentElement.nextSibling;
+    let tick = event.target.parentElement.children[0];
+    if (targetItem.style.textDecoration == "none") {
+      tick.checked = true;
+      targetItem.style.textDecoration = "line-through";
+    } else {
+      tick.checked = false;
+      targetItem.style.textDecoration = "none";
+    }
+  }
+}
+
+// console.log(arrayOfItems);
